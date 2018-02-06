344de0a3f862eaad28fde7097f9eb5a7917e49b7
Williams Medina <williams.medinaa@gmail.com>
2018-02-06 13:07:38 -0700

    chore: start ptoject

---
+++ spec/atom-code-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCodeView from '../lib/atom-code-view';
+
+describe('AtomCodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
